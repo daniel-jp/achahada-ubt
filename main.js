6eecf5767a0ff451fe646e052ce1471dbe8f8b59
Daniel-jp <danny198672@hotmail.com>
2022-11-07 05:15:30 +0100

    Add menu

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
-import './style.css';
-
-import Swiper, { Navigation, Pagination } from 'swiper';
-// import Swiper and modules styles
+import './index.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/swiper-bundle.min.css';
 
+import Swiper from 'swiper';
 
-const navBar = document.querySelector('.menu .navbar')
-const menuBtn = document.querySelector('#menu-btn')
+// import Swiper and modules styles
+
+const navBar = document.querySelector(".menu .navbar");
+const menuBtn = document.querySelector("#menu-btn");
 
 menuBtn.addEventListener('click', () => {
-    menuBtn.classList.toggle('fa-times')
-    navBar.classList.toggle('active')
-})
+    menuBtn.classList.toggle("fa-times");
+    navBar.classList.toggle("active");
+});
+
+
 
 const swiper = new Swiper(".produit-slider", {
     grabCursor: true,
@@ -77,20 +79,20 @@ const swiper3 = new Swiper(".surgel-slide", {
 let flyerPreviewContainer = document.querySelector('.flyer-preview-container')
 let PreviewBox = flyerPreviewContainer.querySelector('.flyer-preview')
 
-document.querySelectorAll('.food .slide').forEach((food) => {
-    flyerPreviewContainer.style.display = 'flex';
+// document.querySelectorAll('.food .slide').forEach((food) => {
+//     flyerPreviewContainer.style.display = 'flex';
 
-    food.onclick = () => {
-        let name = food.getAttribute('data-name');
+//     food.onclick = () => {
+//         let name = food.getAttribute('data-name');
 
-        PreviewBox.forEach(preview => {
-            let target = preview.getAttribute('data-target');
-            if (name == target) {
-                preview.classList.add('active');
-            }
-        })
-    }
-});
+//         PreviewBox.forEach(preview => {
+//             let target = preview.getAttribute('data-target');
+//             if (name == target) {
+//                 preview.classList.add('active');
+//             }
+//         });
+//     };
+// });
 
 flyerPreviewContainer
     .querySelector('#close-preview')
